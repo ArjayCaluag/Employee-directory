547e22201e5cfa4dfffaa4eb9bff9f3023110319
Ron-Arjay Caluag <arjaycaluag@gmail.com>
2021-02-24 13:10:24 -0800

    Create table with static elements

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,14 +1,17 @@
 import "./App.css";
 import React from "react";
 import NavBar from "./components/NavBar";
+import Table from "./components/Table";
 
 function App() {
   // const [employee, setEmployee] = useState({});
 
-  
   return (
     <div>
       <NavBar />
+      <div>
+        <Table />
+      </div>
     </div>
   );
 }
