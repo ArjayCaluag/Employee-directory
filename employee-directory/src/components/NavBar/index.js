064cb63c954c86a51557092cc5af4d5fffacb7d5
Ron-Arjay Caluag <arjaycaluag@gmail.com>
2021-02-24 13:05:02 -0800

    Create initial navBar component

---
+++ employee-directory/src/components/NavBar/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./style.css";
+
+function NavBar(props) {
+  return (
+    <div className = "parent">
+      <nav className="navbar">
+        <ul >
+          <li> Employee Directory</li>
+          <li> Search an employee to filter through table</li>
+          <li>
+           Placeholder
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default NavBar;
