44b724b20493c69b98477903e0cfe0da22c9fbbd
Ron-Arjay Caluag <arjaycaluag@gmail.com>
2021-02-25 09:50:47 -0800

    Use .map to dynamically generate employee table rows

--- employee-directory/src/components/Table/index.js
+++ employee-directory/src/components/Table/index.js
@@ -1,42 +1,36 @@
 import React from "react";
 import "./style.css";
 
-function Table(props) {
-    return (
-        <table class="table">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td>Larry</td>
-            <td>the Bird</td>
-            <td>@twitter</td>
-          </tr>
-        </tbody>
-      </table>
-    );
-  }
+function Table({users}) {
+  console.log(users);
 
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th scope="col">Picture</th>
+          <th scope="col">Name</th>
+          <th scope="col">email</th>
+          <th scope="col">Phone #</th>
+         
+        </tr>
+      </thead>
+      <tbody>
 
+      {users.map(user => (
+        <tr>
+         
+          <td> <img src = {user.picture.large} alt ="userImage" /> </td> 
+          <td>{user.name.first} {user.name.last}</td>
+          <td>{user.email}</td>
+          <td>{user.phone}</td>
+        </tr>
+      ))}
 
+        
+      </tbody>
+    </table>
+  );
+}
 
-export default Table
+export default Table;
